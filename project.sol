a11080976e93738156011754b84a1f92217b7adc
VanshArora08 <kumar.vansharora@gmail.com>
2023-04-11 14:28:46 +0530

    update

--- project.sol
+++ project.sol
@@ -1,9 +1,9 @@
 // SPDX-License-Identifier: MIT
-pragma solidity 0.8.18;
+pragma solidity ^0.8.0;
 
 contract metacrafter {
     address public s_Owner;
-    bool public isOwner = false;
+    uint public price = 0;
 
     constructor() {
         s_Owner = msg.sender;
@@ -13,14 +13,19 @@ contract metacrafter {
         if (msg.sender != s_Owner) {
             revert();
         }
-        isOwner = true;
+
+        price += 10;
     }
 
-    function isOwnerRequire() public view {
+    function isOwnerRequire() public {
         require(s_Owner == msg.sender, "sender is not owner");
+
+        price += 10;
     }
 
-    function isOwnerAssert() public view {
+    function isOwnerAssert() public {
         assert(s_Owner == msg.sender);
+
+        price += 10;
     }
 }
